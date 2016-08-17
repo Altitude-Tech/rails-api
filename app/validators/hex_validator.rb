d4bff37e0af621529a64a57c0d1706e47fed821a
mattdowdell <matthew@altitude.tech>
2016-08-17 15:11:37 +0100

    make hex validator pass rubocop

--- app/validators/hex_validator.rb
+++ app/validators/hex_validator.rb
@@ -3,8 +3,8 @@
 ##
 
 class HexValidator < ActiveModel::EachValidator
-	def validate_each(record, attribute, value)
-		hex_regex = /\A[a-f0-9]+\Z/i
-		record.errors.add attribute, I18n.t(:invalid_hex) unless !!(value =~ hex_regex)
-	end
+  def validate_each(record, attribute, value)
+    hex_regex = /\A[a-f0-9]+\Z/i
+    record.errors.add attribute, I18n.t(:invalid_hex) unless !!(value =~ hex_regex)
+  end
 end
