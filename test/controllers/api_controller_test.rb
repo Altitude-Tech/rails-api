692980367da7dc31641178de65a5dfe4ffafc755
mattdowdell <matthew@altitude.tech>
2016-08-17 15:07:41 +0100

    pass rubocop, use base data hash for all config

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -5,183 +5,161 @@
 require 'test_helper'
 
 class ApiControllerTest < ActionController::TestCase
-	##
-	# Test acceptance of GET request
-	##
-	test 'recognise GET' do
-		assert_recognizes(
-			{controller: 'api', action: 'index'},
-			{path: 'api', method: :get}
-		)
-	end
+  # base data hash to be manipulated as required
+  BASE_DATA = {
+    DID: '5678',
+    DYPE: '1234',
+    LOG_TIME: Time.now.to_i,
+    TEMPERATURE: 25.37,
+    PRESSURE: 1009.30164141,
+    HUMIDITY: 63.1271798896,
+    DATA: {
+      SENSOR_TYPE: SENSOR_MQ2_HASH,
+      SENSOR_ERROR: 0.1,
+      SENSOR_DATA: 47,
+    }
+  }
 
-	##
-	# Test acceptance of POST request
-	##
-	test 'recognise POST' do
-		assert_recognizes(
-			{controller: 'api', action: 'create'},
-			{path: 'api', method: :post}
-		)
-	end
+  ##
+  # Test acceptance of GET request
+  ##
+  test 'recognise GET' do
+    assert_recognizes(
+      { controller: 'api', action: 'index' },
+      { path: 'api', method: :get }
+    )
+  end
 
-	##
-	# Test error handling for no request body
-	##
-	test 'no request body' do
-		post :create
+  ##
+  # Test acceptance of POST request
+  ##
+  test 'recognise POST' do
+    assert_recognizes(
+      { controller: 'api', action: 'create' },
+      { path: 'api', method: :post }
+    )
+  end
 
-		assert_response :bad_request
-		assert_equal 'Missing request body.', response.body
-	end
+  ##
+  # Test error handling for no request body
+  ##
+  test 'no request body' do
+    post :create
 
-	##
-	# Test error handling for missing device id key in JSON request body
-	##
-	test 'missing device id' do
-		data = {
-			:DYPE => '1234',
-			:LOG_TIME => Time.now.to_i,
-			:DATA => {
-				:SENSOR_TYPE => SENSORS['0'],
-				:SENSOR_ERROR => 0.0,
-				:SENSOR_DATA => 10
-			}
-		}
+    expected = 'Missing request body.'
 
-		post :create, body: data.to_json
+    assert_response :bad_request
+    assert_equal(expected, response.body)
+  end
 
-		assert_response :bad_request
-		assert_equal 'Key not found: DID (Device id).', response.body
-	end
+  ##
+  # Test error handling for missing device id key in JSON request body
+  ##
+  test 'missing device id' do
+    data = BASE_DATA.deep_dup.except(:DID)
 
-	##
-	# Test error handling for missing device type key in JSON request body
-	##
-	test 'missing device type' do
-		data = {
-			:DID => '12a4',
-			:LOG_TIME => Time.now.to_i,
-			:DATA => {
-				:SENSOR_TYPE => SENSORS['0'],
-				:SENSOR_ERROR => 0.0,
-				:SENSOR_DATA => 10
-			}
-		}
+    expected = 'Key not found: DID (Device id).'
 
-		post :create, body: data.to_json
+    post :create, body: data.to_json
 
-		assert_equal 'Key not found: DYPE (Device type).', response.body
-	end
+    assert_response :bad_request
+    assert_equal(expected, response.body)
+  end
 
-	##
-	# Test error handling for missing log time key in JSON request body
-	##
-	test 'missing log time' do
-		data = {
-			:DID => '12a4',
-			:DYPE => '1234',
-			:DATA => {
-				:SENSOR_TYPE => SENSORS['0'],
-				:SENSOR_ERROR => 0.0,
-				:SENSOR_DATA => 10
-			}
-		}
+  ##
+  # Test error handling for missing device type key in JSON request body
+  ##
+  test 'missing device type' do
+    data = BASE_DATA.deep_dup.except(:DYPE)
 
-		post :create, body: data.to_json
+    expected = 'Key not found: DYPE (Device type).'
 
-		assert_equal 'Validation failed: Log_time (LOG_TIME) can\'t be blank', response.body
-	end
+    post :create, body: data.to_json
 
-	##
-	# Test error handling for missing data key in JSON request body
-	##
-	test 'missing data' do
-		data = {
-			:DID => '12a4',
-			:DYPE => '1234',
-			:LOG_TIME => Time.now.to_i
-		}
+    assert_equal(expected, response.body)
+  end
 
-		post :create, body: data.to_json
+  ##
+  # Test error handling for missing log time key in JSON request body
+  ##
+  test 'missing log time' do
+    data = BASE_DATA.deep_dup.except(:LOG_TIME)
 
-		assert_equal 'Key not found: DATA (Data).', response.body
-	end
+    expected = 'Validation failed: Log_time (LOG_TIME) can\'t be blank'
 
-	##
-	# Test error handling for missing sensor type key in JSON request body
-	##
-	test 'missing sensor type' do
-		data = {
-			:DID => '12a4',
-			:DYPE => '1234',
-			:LOG_TIME => Time.now.to_i,
-			:DATA => {
-				:SENSOR_ERROR => 0.0,
-				:SENSOR_DATA => 10
-			}
-		}
+    post :create, body: data.to_json
 
-		post :create, body: data.to_json
+    assert_equal(expected, response.body)
+  end
 
-		assert_equal 'Validation failed: Sensor type (SENSOR_TYPE) must be a hexadecimal number.', response.body
-	end
+  ##
+  # Test error handling for missing data key in JSON request body
+  ##
+  test 'missing data' do
+    data = BASE_DATA.deep_dup.except(:DATA)
 
-	##
-	# Test error handling for missing sensor error key in JSON request body
-	##
-	test 'missing sensor error' do
-		data = {
-			:DID => '12a4',
-			:DYPE => '1234',
-			:LOG_TIME => Time.now.to_i,
-			:DATA => {
-				:SENSOR_TYPE => SENSORS['0'],
-				:SENSOR_DATA => 10
-			}
-		}
+    expected = 'Key not found: DATA (Data).'
 
-		post :create, body: data.to_json
+    post :create, body: data.to_json
 
-		assert_equal 'Validation failed: Sensor error (SENSOR_ERROR) must be a number between 0 and 1.', response.body
-	end
+    assert_equal(expected, response.body)
+  end
 
-	##
-	# Test error handling for missing sensor data key in JSON request body
-	##
-	test 'missing sensor data' do
-		data = {
-			:DID => '12a4',
-			:DYPE => '1234',
-			:LOG_TIME => Time.now.to_i,
-			:DATA => {
-				:SENSOR_TYPE => SENSORS['0'],
-				:SENSOR_ERROR => 0.0
-			}
-		}
+  ##
+  # Test error handling for missing sensor type key in JSON request body
+  ##
+  test 'missing sensor type' do
+    data = BASE_DATA.deep_dup
+    data[:DATA].delete(:SENSOR_TYPE)
 
-		post :create, body: data.to_json
+    expected = 'Validation failed: Sensor type (SENSOR_TYPE) is an ' \
+               'unrecognised value.'
 
-		assert_equal 'Validation failed: Sensor data (SENSOR_DATA) must be a number greater than or equal to 0.', response.body
-	end
+    post :create, body: data.to_json
 
-	##
-	# Test successful creation
-	##
-	test 'successful create' do
-		data = {
-			:DID => '12a4',
-			:DYPE => '1234',
-			:LOG_TIME => Time.now.to_i,
-			:DATA => {
-				:SENSOR_TYPE => SENSORS['0'],
-				:SENSOR_ERROR => 0.0,
-				:SENSOR_DATA => 10
-			}
-		}
+    assert_equal(expected, response.body)
+  end
 
-		post :create, body: data.to_json
+  ##
+  # Test error handling for missing sensor error key in JSON request body
+  ##
+  test 'missing sensor error' do
+    data = BASE_DATA.deep_dup
+    data[:DATA].delete(:SENSOR_ERROR)
 
-		assert_equal 'Data inserted successfully.', response.body
-	end
+    post :create, body: data.to_json
+
+    expected = 'Validation failed: Sensor error (SENSOR_ERROR) must be a ' \
+               'number between 0 and 1.'
+
+    assert_equal(expected, response.body)
+  end
+
+  ##
+  # Test error handling for missing sensor data key in JSON request body
+  ##
+  test 'missing sensor data' do
+    data = BASE_DATA.deep_dup
+    data[:DATA].delete(:SENSOR_DATA)
+
+    expected = 'Validation failed: Sensor data (SENSOR_DATA) must be a ' \
+               'number greater than or equal to 0.'
+
+    post :create, body: data.to_json
+
+    assert_equal(expected, response.body)
+  end
+
+  ##
+  # Test successful creation
+  ##
+  test 'successful create' do
+    data = BASE_DATA.deep_dup
+
+    expected = 'Data inserted successfully.'
+
+    post :create, body: data.to_json
+
+    assert_equal(expected, response.body)
+  end
 end
