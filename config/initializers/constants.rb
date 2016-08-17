c803f48f2695cf0b43355c02327b62047584f2c4
mattdowdell <matthew@altitude.tech>
2016-08-17 15:09:08 +0100

    merge constants into a single file

---
+++ config/initializers/constants.rb
@@ -0,0 +1,29 @@
+##
+# Constants
+##
+
+require 'digest'
+
+##
+# Gases
+##
+GAS_CARBON_MONOXIDE = 0
+GAS_NITROGEN_DIOXIDE = 1
+GAS_ALCOHOLS = 2
+GAS_SMOKE = 3
+GAS_TOXIC_GASES = 4
+GAS_PM10 = 5
+GAS_PM25 = 6
+
+##
+# Sensors
+##
+SENSOR_MQ2 = 'mq2'
+SENSOR_MQ7 = 'mq7'
+SENSOR_MQ135 = 'mq135'
+
+SENSOR_MQ2_HASH = Digest::SHA1.hexdigest(SENSOR_MQ2)
+SENSOR_MQ7_HASH = Digest::SHA1.hexdigest(SENSOR_MQ7)
+SENSOR_MQ135_HASH = Digest::SHA1.hexdigest(SENSOR_MQ135)
+
+SENSOR_HASHES = [SENSOR_MQ2_HASH, SENSOR_MQ7_HASH, SENSOR_MQ135_HASH].freeze
