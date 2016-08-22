347d1584a14dd8b78e19204276578071f4e3202b
mattdowdell <matthew@altitude.tech>
2016-08-22 11:57:27 +0100

    truncate test.log before running tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,16 @@
 ENV['RAILS_ENV'] ||= 'test'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+# empty log file before running tests
+log_file = File.expand_path('../../log/test.log', __FILE__)
+
+if File.file?(log_file)
+  File.open(log_file, 'w') {}
+end
+
+# make sure database is up to date before running tests
 ActiveRecord::Migration.maintain_test_schema!
 
 ##
