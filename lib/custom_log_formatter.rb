1e81baa031d5d6c7f425bf575cfc3ebf5ef92f28
mattdowdell <mdowdell244@gmail.com>
2016-08-25 08:45:08 +0100

    add docs for log formatter, inspect non strings

--- lib/custom_log_formatter.rb
+++ lib/custom_log_formatter.rb
@@ -1,12 +1,25 @@
 ##
-#
+# Customised logging formatter
 ##
 class CustomLogFormatter < Logger::Formatter
   ##
+  # Sets the format of the log message
   #
+  # Outputs the time, severity and message in development and production
+  # Outputs the severity and message in test
   ##
   def call(severity, time, program_name, message)
+    message = message_to_s(message)
+
     return "[#{severity}] #{message}\n" if ENV['RAILS_ENV'] == 'test'
     return "[#{time}] [#{severity}] #{message}\n"
+  end
+
+  ##
+  # Format the message object to handle non-strings being logged
+  ##
+  def message_to_s(obj)
+    return "#{obj}" if obj.is_a?(String)
+    return obj.inspect
   end
 end
