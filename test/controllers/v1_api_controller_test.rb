acea976b1ba30fbd60d1313cf1fb90aee1479afa
mattdowdell <matthew@altitude.tech>
2016-08-25 16:20:36 +0100

    test standard error handling (with a ridiculous hack)

--- test/controllers/v1_api_controller_test.rb
+++ test/controllers/v1_api_controller_test.rb
@@ -17,7 +17,7 @@ class V1ApiControllerTest < ActionController::TestCase
   end
 
   ##
-  #
+  # Test conversion of a hash with a hash
   ##
   test 'normalize hash nested' do
     input = { 'a' => { 'a' => 1, 'b' => 2 } }
@@ -28,7 +28,7 @@ class V1ApiControllerTest < ActionController::TestCase
   end
 
   ##
-  #
+  # Test conversion of an array within a hash
   ##
   test 'normalize hash nested array' do
     input = { 'a' => [1, 2, 3] }
@@ -39,7 +39,7 @@ class V1ApiControllerTest < ActionController::TestCase
   end
 
   ##
-  #
+  # Test conversion of a hash withion an array within a hash
   ##
   test 'normalize hash nested array hashes' do
     input = { 'a' => [{ 'a' => 1, 'b' => 2 }] }
@@ -47,5 +47,70 @@ class V1ApiControllerTest < ActionController::TestCase
 
     actual = V1ApiController.new.send(:normalize_keys, input)
     assert_equal(expected, actual)
+  end
+
+  ##
+  # Test error handling of StandardError in test mode
+  ##
+  test 'handle standard error test' do
+    exc = StandardError.new('test')
+
+    assert_raises(StandardError) do
+      V1ApiController.new.send(:standard_error, exc)
+    end
+  end
+
+  ##
+  # Test error handling of StandardError in development mode
+  #
+  # What's probably meant to happen here is mock the controller somehow
+  # but instead, this sends the expected argument to the standard_error method
+  # then catches the expected error.
+  #
+  # After that, the error message is tested to get the error message.
+  ##
+  test 'handle standard error development' do
+    exc = StandardError.new('test')
+    error_regex = /@error="(.*?)"/
+    msg = I18n.t('controller.v1.error.unhandled_error')
+
+    old_env = ENV['RAILS_ENV']
+    ENV['RAILS_ENV'] = 'production'
+
+    begin
+      V1ApiController.new.send(:standard_error, exc)
+    rescue Module::DelegationError => e
+      error_match = error_regex.match(e.message)
+      assert_equal(error_match.captures[0], msg)
+    end
+
+    ENV['RAILS_ENV'] = 'test'
+  end
+
+  ##
+  # Test error handling of StandardError in production mode
+  #
+  # What's probably meant to happen here is mock the controller somehow
+  # but instead, this sends the expected argument to the standard_error method
+  # then catches the expected error.
+  #
+  # After that, the error message is tested to get the error message.
+  ##
+  test 'handle standard error production' do
+    exc = StandardError.new('test')
+    error_regex = /@error="(.*?)"/
+    msg = I18n.t('controller.v1.error.unhandled_error')
+
+    old_env = ENV['RAILS_ENV']
+    ENV['RAILS_ENV'] = 'production'
+
+    begin
+      V1ApiController.new.send(:standard_error, exc)
+    rescue Module::DelegationError => e
+      error_match = error_regex.match(e.message)
+      assert_equal(error_match.captures[0], msg)
+    end
+
+    ENV['RAILS_ENV'] = 'test'
   end
 end
