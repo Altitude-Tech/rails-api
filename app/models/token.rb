9a1e3b4fcdee5ded7ecbb7db8a98e44d7aa20026
mattdowdell <matthew@altitude.tech>
2016-09-05 17:21:31 +0100

    add finalised db layout for tokens

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,42 +1,45 @@
 ##
 #
 ##
-class Key < ApplicationRecord
+class Token < ApplicationRecord
+  ##
+  # Callbacks
+  ##
+  before_validation(:generate_token)
+
   ##
   # Validations
   ##
-  validates(:public_key, uniqueness: true)
-  validates(:private_key, presence: true)
-  validates(:type, presence: true)
-  validates(:expires, presence: true)
-  validates(:valid, presence: true)
-
-  ##
-  # Constants
-  ##
-
-  # api tokens for... (developers?)
-  # device tokens for registering devices (use qr codes?)
-  # email tokens for confirming an email change/registration (figure out how to deal with forgotten email)
-  # session tokens for sessions in sensly app/website
-  # password reset tokens for resetting a password
-
-  # @todo figure out public & private key for hmac
-
-  TYPE_API = 1
-  TYPE_DEVICE = 2
-
-  TYPES = [TYPE_API, TYPE_DEVICE].freeze
+  validates(:token, uniqueness: true)
+  # validates(:expires, allow_nil: true, token_expiry: true)
+  validates(:enabled, presence: true)
 
   ##
   #
   ##
-  def generate_keys
+  def disable!
+    self[:enabled] = false
+    self.save!
+  end
+
+  ##
+  #
+  ##
+  def generate_token
+    self[:token] = SecureRandom.hex
   end
 
   ##
   #
   ##
   def enabled?
+    return self[:enabled]
+  end
+
+  ##
+  #
+  ##
+  def expired?
+    return self[:expires] > Time.now.utc
   end
 end
--- db/migrate/20160831212927_create_tokens.rb
+++ db/migrate/20160831212927_create_tokens.rb
@@ -1,10 +1,9 @@
 class CreateTokens < ActiveRecord::Migration[5.0]
   def change
     create_table :tokens do |t|
-      t.string :public_key, null: false
-      t.string :private_key, null: false
-      t.datetime :expires, null: false
-      t.boolean :valid, null: false
+      t.string :token, null: false
+      t.datetime :expires
+      t.boolean :enabled, null: false
 
       t.timestamps
     end
