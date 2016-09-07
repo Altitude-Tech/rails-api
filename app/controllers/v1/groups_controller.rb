ee6ccc60256e68458820aa019b458978d776b15a
mattdowdell <mdowdell244@gmail.com>
2016-09-07 08:49:33 +0100

    start groups controller

---
+++ app/controllers/v1/groups_controller.rb
@@ -0,0 +1,45 @@
+##
+#
+##
+
+require 'exceptions'
+
+##
+#
+##
+module V1
+  ##
+  #
+  ##
+  class GroupsController < V1ApiController
+    ##
+    # Create a new group
+    ##
+    def create
+    end
+
+    ##
+    # Get a list of groups
+    ##
+    def index
+    end
+
+    ##
+    # Get details about a specific group
+    ##
+    def show
+    end
+
+    ##
+    # Add users to a group
+    ##
+    def add_users
+    end
+
+    ##
+    # Remove users from a group
+    ##
+    def remove_users
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,27 @@
+##
+# Routes configuration
+#
+# The application will need to be reloaded after changes for them to take effect
+##
+
 Rails.application.routes.draw do
   # api v1
   namespace :v1 do
     # simple routes
     resources :data, only: [:create]
     resources :devices, only: [:create, :index, :show]
+    resources :groups, only: [:create, :index, :show]
     resources :users, only: [:create, :index]
 
     # extra data routes
     controller :data do
       match 'data/:device_id', to: 'data#show', via: :get
+    end
+
+    # extra groups routes
+    controller :groups do
+      match 'groups/add_users', to: 'groups#add_users', via: :post
+      match 'groups/remove_users', to: 'groups#remove_users', via: :post
     end
 
     # extra users routes
