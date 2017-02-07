8d3b3255dc0d15c9cf6b1d91252542dbc42744c9
mattdowdell <mdowdell244@gmail.com>
2017-02-07 17:50:05 +0000

    remove debugging method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -3,16 +3,4 @@
 ##
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  protected
-
-  ##
-  # Method for debugging type casting.
-  #
-  # To use, add `before_validation :debug_type_casting` to a model class.
-  ##
-  def debug_type_casting
-    Rails.logger.debug "before: #{attributes_before_type_cast}"
-    Rails.logger.debug "after: #{attributes}"
-  end
 end
